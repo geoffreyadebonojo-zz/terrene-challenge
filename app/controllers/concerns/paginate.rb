3dc927449ce77efd88d31e8c12803dc268b1cb3d
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2021-06-03 09:53:56 -0600

    Create simple pagination test

--- app/controllers/concerns/paginate.rb
+++ app/controllers/concerns/paginate.rb
@@ -1,7 +1,7 @@
 module Paginate
   def paginate(object, args=nil)
     return object if args.nil?
-    
+
     page = args[:page] || 1
     results_per_page = args[:results_per_page] || object.count
     object.page(page).per(results_per_page)
--- spec/requests/v1/items_spec.rb
+++ spec/requests/v1/items_spec.rb
@@ -11,27 +11,53 @@ RSpec.describe 'Items API', type: :request do
 
   # Test suite for GET /todos/:todo_id/items
   describe 'GET /v1/todos/:todo_id/items' do
-    before { get "/v1/todos/#{todo_id}/items", headers: headers }
 
-    context 'when todo exists' do
+    context 'without pagination' do
+      before { get "/v1/todos/#{todo_id}/items", headers: headers }
+
+      context 'when todo exists' do
+        it 'returns status code 200' do
+          expect(response).to have_http_status(200)
+        end
+
+        it 'returns all todo items' do
+          expect(json.size).to eq(20)
+        end
+      end
+
+      context 'when todo does not exist' do
+        let(:todo_id) { 0 }
+
+        it 'returns status code 404' do
+          expect(response).to have_http_status(404)
+        end
+
+        it 'returns a not found message' do
+          expect(response.body).to match(/Couldn't find Todo/)
+        end
+      end
+    end
+
+    context 'test pagination' do
+      let!(:todo1) { create(:todo, created_by: user.id) }
+      let!(:items1) { create_list(:item, 20, todo_id: todo1.id) }
+      before { get "/v1/todos/#{todo_id}/items", params: {page: 1, results_per_page: 5}, headers: headers }
+
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
 
-      it 'returns all todo items' do
-        expect(json.size).to eq(20)
-      end
-    end
-
-    context 'when todo does not exist' do
-      let(:todo_id) { 0 }
-
-      it 'returns status code 404' do
-        expect(response).to have_http_status(404)
+      it 'returns correct number of items' do
+        expect(json.size).to eq(5)
       end
 
-      it 'returns a not found message' do
-        expect(response.body).to match(/Couldn't find Todo/)
+      it 'returns correct page of items' do
+        first_page = json
+
+        get "/v1/todos/#{todo_id}/items", params: {page: 2, results_per_page: 5}, headers: headers
+        second_page = json
+
+        expect(second_page).to_not eq(first_page)
       end
     end
   end
